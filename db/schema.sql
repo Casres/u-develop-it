7924a2551883df8ec3dd5c247d2537b0151f888c
Casres <christian@casillas.tech>
2022-05-21 20:52:19 -0400

    AskBCS SP/ MySQL problems

---
+++ db/schema.sql
@@ -0,0 +1,8 @@
+-- this handles the creation of tables in the DataBase 
+-- this is the structure of the tables
+CREATE TABLE candidates (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  industry_connected BOOLEAN NOT NULL
+);
