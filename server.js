dbaa6fb7708e58cebfad49c22d929d1ca65c1bb1
Casres <christian@casillas.tech>
2022-05-25 19:20:02 -0400

    12.2.8

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 // express is for servers
-const express = require("express");
+const express = require('express');
+const res = require('express/lib/response');
 // MySQL is for data bases
-const mysql = require("mysql2");
+const mysql = require('mysql2');
+const inputCheck = require('./utils/inputCheck')
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -9,16 +11,6 @@ const app = express();
 // express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-// this starts the express server
-app.listen(PORT, () => {
-  console.log(`Server ONLINE listening to ${PORT}`);
-});
-
-// for when the user makes a GET request to the server but that isn't supported by the app
-app.use((req, res) => {
-  res.status(404).end();
-});
 
 // when the user connects to the server, they will see this message
 app.get("/", (req, res) => {
@@ -29,14 +21,15 @@ app.get("/", (req, res) => {
 const db = mysql.createConnection(
   // 127.0.0.1
   { 
-    host: "127.0.0.1",
+    host: "localhost",
     user: "root",
-    password: "",
+    password: "MidKnightSun",
     database: "election",
   },
   console.log("CONNECTED to 'election' DataBase")
 );
 
+// shows the error // error handling
 db.connect((err) => {
   if (err) {
     throw err;
@@ -55,30 +48,70 @@ app.get("/api/candidates", (req, res) => {
   });
 });
 
-// // selects and shows the row with id 1
-// db.query(`SELECT * FROM candidates WHERE ID = 1`, (err, rows) => {
-//   if (err) {
-//     console.log(err, "id number not found");
-//   }
-//   console.log(rows);
-// });
+// selects and shows the row with id 1
+app.get('/api/candidates/:id', (req, res) => {
+  const sql = `SELECT * FROM candidates WHERE id = ?`;
+  const params = [req.params.id]; 
 
-// // deletes candidate
-// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, results) => {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log(results);
-// });
+  db.query(sql, params, (err, rows) => {
+    if (err) {
+      res.status(400).json({error: err.message});
+      return;
+    }
+    res.json({
+      data: rows,
+      message: "success"
+    });
+  });
+});
 
-// // creates a candidate
-// const sql = 'INSERT INTO candidates (id, first_name, last_name, industry_connected) VALUES (?,?,?,?)';
+// deletes selected candidate
+app.delete('/api/candidates/:id', (req, res) => {
+  const sql = `DELETE FROM candidates WHERE id = ?`;
+  const params = [req.params.id];
 
-// const params = [1, 'Ronald', 'Firbank', 1];
+  db.query(sql, params, (err, results) => {
+    if (err) {
+      res.statusMessage(400).json({error: err.message});
+      return;
+    } else if (!results.affectedRows) {
+      res.json({message: "Candidate NOT found, please try again"});
+    } else {
+      res.json({
+        data: results.affectedRows,
+        id: req.params.id,
+        message: "deleted successfully"
+      });
+    }
+  });
+});
 
-// db.query(sql, params, (err, results) => {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log(results);
-// });
+// creates a candidate
+app.post('/api/candidates', ({body}, res) => {
+  const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+  if (errors) {
+    res.status(400).json({error: errors});
+    return;
+  }
+  const sql = `INSERT INTO candidates (first_name, last_name, industry_connected) VALUES (?,?,?)`;
+  const params = [body.first_name, body.last_name, body.industry_connected];
+
+  db.query(sql, params, (err, results) => {
+    if (err) {
+      res.status(400).json({error: err.message});
+      return;
+    }
+    res.json({data:body, message: "successfully added candidate"});
+    return;
+  });
+});
+
+// this starts the express server
+app.listen(PORT, () => {
+  console.log(`Server ONLINE listening to ${PORT}`);
+});
+
+// for when the user makes a GET request to the server but that isn't supported by the app
+app.use((req, res) => {
+  res.status(404).end();
+});
